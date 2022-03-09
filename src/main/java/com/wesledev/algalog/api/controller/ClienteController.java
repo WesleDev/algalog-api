646073a0ff354f9b61368fc2e67540cebe8d88ca
WesleDev <wesle.profissional@gmail.com>
2022-03-09 14:42:33 -0300

    Usando Jarkarta Persistence

--- src/main/java/com/wesledev/algalog/api/controller/ClienteController.java
+++ src/main/java/com/wesledev/algalog/api/controller/ClienteController.java
@@ -1,29 +1,24 @@
 package com.wesledev.algalog.api.controller;
 
-import com.wesledev.algalog.domain.model.Cliente;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
+import com.wesledev.algalog.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;
 
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Wesle2");
-        cliente1.setEmail("teste@teste.com");
-        cliente1.setTelefone("999999999999");
-
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Joao");
-        cliente2.setEmail("teste@teste.com.br");
-        cliente2.setTelefone("999999999999");
-
-        return Arrays.asList(cliente1, cliente2);
+       return manager.createQuery("from Cliente", Cliente.class)
+    		   .getResultList();
     }
 }
--- src/main/java/com/wesledev/algalog/domain/model/Cliente.java
+++ src/main/java/com/wesledev/algalog/domain/model/Cliente.java
@@ -1,13 +1,47 @@
 package com.wesledev.algalog.domain.model;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+	
     private String nome;
+	
     private String email;
+    
+    @Column(name =  "fone")
     private String telefone;
+    
+    
 
-    public Long getId() {
+    @Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	public Long getId() {
         return id;
     }
 
